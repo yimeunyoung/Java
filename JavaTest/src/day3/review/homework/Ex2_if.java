bfff11cf099bccd32f8300d68be51f0f5fdb3969
yimeunyoung <kil_yea@hanmail.net>
2023-06-02 21:31:39 +0900

    Ex2_if

---
+++ JavaTest/src/day3/review/homework/Ex2_if.java
@@ -0,0 +1,25 @@
+package day3.review.homework;
+
+import java.util.Scanner;
+
+public class Ex2_if {
+
+	public static void main(String[] args) {
+		/*성별(M:남성, W:여성)를 입력받아 여성인지 확인하는 코드를 작성하세요.
+		 * 조건 연산자 이용
+		 * input gender : 
+		 * M
+		 * Are you a women? man
+		 * input gender : 
+		 * W
+		 * Are you a women?  woman
+		* */
+		Scanner sc = new Scanner(System.in);
+		String gender = man;
+		System.out.println(" Are you a women? ");
+		
+		sc.close();
+
+	}
+
+}
