e455d40b9a6e0c66a6f969286c4ef2030b88839e
yimeunyoung <kil_yea@hanmail.net>
2023-06-04 23:28:58 +0900

    홀짝 판별 - 조건 연산자를 이용하여 작성

---
+++ JavaTest/src/Eclipse - 은영/JavaTest/src/day4/review/ifex/EvenNumberEx2.java
@@ -0,0 +1,16 @@
+package day4.review.ifex;
+
+import java.util.Scanner;
+
+public class EvenNumberEx2 {
+
+	public static void main(String[] args) {
+		//홀짝 판별 예제를 조건 연산자를 이용하여 작성
+		int num = 6;
+		String result = num % 2 == 0 ? "even" : "odd";
+		System.out.println(num + " is an " + result + " number. ");
+		
+		
+	}
+
+}
