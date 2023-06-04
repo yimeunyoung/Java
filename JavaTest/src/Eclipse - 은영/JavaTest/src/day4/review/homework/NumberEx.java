a9cbe4702f7aacfd44d933dc11ded3ed78c81c99
yimeunyoung <kil_yea@hanmail.net>
2023-06-04 23:32:43 +0900

    정수 - 짝수/홀수

---
+++ JavaTest/src/Eclipse - 은영/JavaTest/src/day4/review/homework/NumberEx.java
@@ -0,0 +1,13 @@
+package day4.review.homework;
+
+public class NumberEx {
+
+	public static void main(String[] args) {
+		//정수 num가 짝수이면 num에 2를 나누고, 
+		//정수 num가 홀수이면 num에 1을 더한 후, 
+		//num를 출력하는 코드를 작성하세요.
+
+		
+	}
+
+}
